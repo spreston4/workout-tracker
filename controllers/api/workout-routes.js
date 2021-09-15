c7b4aa51a58e3364455b1028f0599e48294b7102
spreston <sam.preston11@gmail.com>
2021-09-15 17:46:46 -0500

    Added route to creat new workout.

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -2,7 +2,17 @@
 const router = require('express').Router();
 const Workout = require('../../models/Workout.js');
 
-// Create a workout
+// Create a new workout
+router.post('/', async (req, res) => {
+    try {
+
+        const workoutData = await db.Workout.create(req.body);
+        res.status(200).json(workoutData);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // Edit a workout by adding an exercise
 
