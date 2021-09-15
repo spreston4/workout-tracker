a5fbfeb6ae2af5a93af0a7d3e2d348a78db87a8c
spreston <sam.preston11@gmail.com>
2021-09-15 17:26:18 -0500

    Shell for workout-routes.

---
+++ controllers/api/workout-routes.js
@@ -0,0 +1,15 @@
+// Require dependencies
+const router = require('express').Router();
+const Workout = require('../../models/Workout.js');
+
+// Create a workout
+
+// Edit a workout by adding an exercise
+
+// Get last workout
+
+// Get workouts in range
+
+
+// Export
+module.exports = router;
